30d98522cc0328dd84a7bc85b3e65a2c71b27f00
Adam Du <dulinnan@gmail.com>
2018-11-11 15:17:31 +1300

    update

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+<script>
+/* ============
+ * Entry Point
+ * ============
+ *
+ * The entry point of the application.
+ */
+
+
+export default {
+  /**
+   * The name of the application.
+   */
+  name: 'RuralHoliday',
+
+  /**
+   * Fires when the app has been mounted.
+   */
+  mounted() {
+    // If the user is authenticated,
+    // fetch the data from the API
+    if (this.$store.state.auth.authenticated) {
+      this.$store.dispatch('account/find');
+    }
+  },
+};
+</script>
+
+<style>
+    /*#app{*/
+        /*font-family: "Lato", "Helvetica Condensed Light", "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", sans-serif;*/
+    /*}*/
+</style>
